b011a1d77ed6ea42e18d3507fd19d69481223000
Jmilk3 <jmilk@g.hmc.edu>
2025-11-16 23:03:20 -0800

    forgot to add file

---
+++ Code/asheboro_tests.py
@@ -0,0 +1,22 @@
+"""
+A file which contains the tests based on Asheboro election data
+"""
+from test_params import TestParams
+from bloc import Bloc
+
+### Board of EDU
+## Black voters vs Non-black voters
+# candidate indifference
+
+# 
+
+## White voters vs non-white voters
+
+## Hispanic voters vs non-hispanic voters
+
+### City Council
+## Black voters vs Non-black voters
+
+## White voters vs non-white voters
+
+## Hispanic voters vs non-hispanic voters
