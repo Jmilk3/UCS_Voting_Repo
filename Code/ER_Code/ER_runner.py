8404523c6a94f8b9fd2808a79c74d4df0286af31
LittleLudroth <joaquin.milk@protonmail.com>
2026-01-28 10:30:31 -0800

    removed out of date er function and polished comments

--- Code/ER_Code/ER_runner.py
+++ Code/ER_Code/ER_runner.py
@@ -20,7 +20,7 @@ def env_reg(election_file, registration_file, condition_code, condition_col = "r
     election_data = read_csv(election_file, dtype={"Precinct": str})
     reg_data = read_csv(registration_file,  dtype={"precinct_abbrv": str})
 
-    # TODO: Set column names for various paramaters
+    # Set column names for various paramaters
     ELECTION_CANDIDATE_COLUMN = "Choice" # Name of the column in election_data which has candidate names
     ELECTION_PRECINCT_COLUMN = "Precinct" # Name of the column in election_data which has the precinct information
     ELECTION_VOTE_COLUMN = "Total Votes" # Name of the column in election_data which has the number of votes
@@ -29,6 +29,7 @@ def env_reg(election_file, registration_file, condition_code, condition_col = "r
 
     # Get a list of precincts and candidates for the election
     # NOTE: This assumes that unwanted candidates, such as OVER VOTE or WRITE IN, are already removed
+    # It also assumes that the precinct designations are the same in both the election and reg data
     candidates = election_data[ELECTION_CANDIDATE_COLUMN].unique()
     precincts = election_data[ELECTION_PRECINCT_COLUMN].unique().tolist()
 
@@ -37,7 +38,7 @@ def env_reg(election_file, registration_file, condition_code, condition_col = "r
         sum(election_data[election_data[ELECTION_PRECINCT_COLUMN] == precinct][ELECTION_VOTE_COLUMN].values) != 0,
         precincts))
     
-    # Remove any precincts missing from reg file
+    # Remove any precincts that are missing from reg file
     reg_precincts = reg_data[REG_PRECINCT_COLUMN].unique()
     precincts = list(filter(lambda precinct:
                             precinct in reg_precincts,
@@ -49,9 +50,8 @@ def env_reg(election_file, registration_file, condition_code, condition_col = "r
         sum(reg_data[reg_data[REG_PRECINCT_COLUMN] == precinct][REG_VOTE_COLUMN].values),
         precincts)))
             
-    # Get the percentage of voters that meet the given condition in each precinct
-    # TODO: Select a condition that defines your desired group
-    voter_data = reg_data[reg_data[condition_col] == condition_code] # This condition gets all voters with race_code B
+    # Get data for voters that meet the given condition in each precinct
+    voter_data = reg_data[reg_data[condition_col] == condition_code]
 
     # Calculate the percentage of voters that meet your condition by precinct
     voter_percent = array(list(map(lambda precinct, total_votes: 
@@ -87,57 +87,54 @@ def env_reg(election_file, registration_file, condition_code, condition_col = "r
         print(ER_plot.summary())
 
         # plot the graph
-        # NOTE: Plot won't display anything if run from terminal. Use savefig to store the result as a file instead.
+        # NOTE: Currently saves graph as png in active directory.
         ER_plot.plot().set_title(f"{election_name}: {candidate} ({group_name})")
-
-        # TODO: Uncomment this and customize the file name to store graphs
         savefig(f"{election_name}_{candidate}_{group_name}.png")
-        close() # TODO: Remove this if you want the graph to stay open
+        close()
 
 if __name__ == "__main__":
     # Asheboro Board of EDU
-    # env_reg(Path("Asheboro ER/asheboro_board_of_edu.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
-    #         "B", election_name="Asheboro_BoE", group_name="Black")
-    # env_reg(Path("Asheboro ER/asheboro_board_of_edu.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
-    #         "A", election_name="Asheboro_BoE", group_name="Asian")
-    # env_reg(Path("Asheboro ER/asheboro_board_of_edu.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
-    #         "W", election_name="Asheboro_BoE", group_name="White")
-    # env_reg(Path("Asheboro ER/asheboro_board_of_edu.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
-    #         "HL", condition_col="ethnic_code", election_name="Asheboro_BoE", group_name="Hispanic")
+    env_reg(Path("Asheboro ER/asheboro_board_of_edu.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
+            "B", election_name="Asheboro_BoE", group_name="Black")
+    env_reg(Path("Asheboro ER/asheboro_board_of_edu.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
+            "A", election_name="Asheboro_BoE", group_name="Asian")
+    env_reg(Path("Asheboro ER/asheboro_board_of_edu.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
+            "W", election_name="Asheboro_BoE", group_name="White")
+    env_reg(Path("Asheboro ER/asheboro_board_of_edu.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
+            "HL", condition_col="ethnic_code", election_name="Asheboro_BoE", group_name="Hispanic")
 
 
     # Asheboro City Council
-    # env_reg(Path("Asheboro ER/asheboro_city_council.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
-    #         "B", election_name="Asheboro_Council", group_name="Black")
-    # env_reg(Path("Asheboro ER/asheboro_city_council.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
-    #         "A", election_name="Asheboro_Council", group_name="Asian")
-    # env_reg(Path("Asheboro ER/asheboro_city_council.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
-    #         "W", election_name="Asheboro_Council", group_name="White")
-    # env_reg(Path("Asheboro ER/asheboro_city_council.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
-    #         "HL", condition_col="ethnic_code", election_name="Asheboro_Council", group_name="Hispanic")
+    env_reg(Path("Asheboro ER/asheboro_city_council.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
+            "B", election_name="Asheboro_Council", group_name="Black")
+    env_reg(Path("Asheboro ER/asheboro_city_council.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
+            "A", election_name="Asheboro_Council", group_name="Asian")
+    env_reg(Path("Asheboro ER/asheboro_city_council.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
+            "W", election_name="Asheboro_Council", group_name="White")
+    env_reg(Path("Asheboro ER/asheboro_city_council.csv").resolve(), Path("Asheboro ER/Asheboro_Registration_Data.csv").resolve(),
+            "HL", condition_col="ethnic_code", election_name="Asheboro_Council", group_name="Hispanic")
 
     # for the smithfield town council at large
-    # env_reg(Path("Smithfield ER/smithfield_town_council.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
-    #         "B", election_name="Smithfield_Council", group_name="Black")
-    # env_reg(Path("Smithfield ER/smithfield_town_council.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
-    #         "A", election_name="Smithfield_Council", group_name="Asian")
-    # env_reg(Path("Smithfield ER/smithfield_town_council.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
-    #         "W", election_name="Smithfield_Council", group_name="White")
-    # env_reg(Path("Smithfield ER/smithfield_town_council.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
-    #          "HL", condition_col="ethnic_code", election_name="Smithfield_Council", group_name="Hispanic")
+    env_reg(Path("Smithfield ER/smithfield_town_council.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
+            "B", election_name="Smithfield_Council", group_name="Black")
+    env_reg(Path("Smithfield ER/smithfield_town_council.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
+            "A", election_name="Smithfield_Council", group_name="Asian")
+    env_reg(Path("Smithfield ER/smithfield_town_council.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
+            "W", election_name="Smithfield_Council", group_name="White")
+    env_reg(Path("Smithfield ER/smithfield_town_council.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
+             "HL", condition_col="ethnic_code", election_name="Smithfield_Council", group_name="Hispanic")
 
     # for the town of smithfield mayor
-    # env_reg(Path("Smithfield ER/smithfield_mayor.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
-    #         "B", election_name="Smithfield_Mayor", group_name="Black")
-    # env_reg(Path("Smithfield ER/smithfield_mayor.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
-    #         "A", election_name="Smithfield_Mayor", group_name="Asian")
-    # env_reg(Path("Smithfield ER/smithfield_mayor.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
-    #         "W", election_name="Smithfield_Mayor", group_name="White")
-    # env_reg(Path("Smithfield ER/smithfield_mayor.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
-    #        "HL", condition_col="ethnic_code", election_name="Smithfield_Mayor", group_name="Hispanic")
+    env_reg(Path("Smithfield ER/smithfield_mayor.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
+            "B", election_name="Smithfield_Mayor", group_name="Black")
+    env_reg(Path("Smithfield ER/smithfield_mayor.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
+            "A", election_name="Smithfield_Mayor", group_name="Asian")
+    env_reg(Path("Smithfield ER/smithfield_mayor.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
+            "W", election_name="Smithfield_Mayor", group_name="White")
+    env_reg(Path("Smithfield ER/smithfield_mayor.csv").resolve(), Path("Smithfield ER/Smithfield_Registration_Data.csv").resolve(),
+           "HL", condition_col="ethnic_code", election_name="Smithfield_Mayor", group_name="Hispanic")
     
     # for the Charlotte board of education
-    # TODO: FIGURE OUT WHY THIS WON'T RUN!!!!
     env_reg(Path("Charlotte ER/charlotte_board_of_education.csv").resolve(), Path("Charlotte ER/Charlotte_Registration_Data_2023.csv").resolve(),
             "B", election_name="Charlotte_BoE", group_name="Black")
     env_reg(Path("Charlotte ER/charlotte_board_of_education.csv").resolve(), Path("Charlotte ER/Charlotte_Registration_Data_2023.csv").resolve(),
