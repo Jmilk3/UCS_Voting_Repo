78bb350b7fa4a034b60e6623db37b63640bb7d6a
Jmilk3 <jmilk@g.hmc.edu>
2026-02-08 13:38:06 -0800

    started on smithfield

--- Code/Simulations/definitions/smithfield_sims.py
+++ Code/Simulations/definitions/smithfield_sims.py
@@ -6,7 +6,21 @@ from structures.bloc import Bloc
 
 ### Mayor
 ## Black voters vs Non-black voters
-Mayor_Black = SimParams()
+# Black Voter Sum = 1.547
+# Other Voter Sum = 0.887
+Mayor_Black = SimParams("Smithfield Council Black",
+                        Bloc("Black Voters",
+                             0.26221453287197233,
+                             ["Doris Louise Wallace", "Felicia C. Baxter", "Stuart Ashby Lee"],
+                             {"Black Voters": 0.635, "Other Voters": 0.365},
+                             {"Black Voters": {"Doris Louise Wallace": [0.0350471  0.83714949], "Felicia C. Baxter": [0.05430359 0.54919671], "Stuart Ashby Lee": [0.02759562 0.39267972]},
+                               "Other Voters": {"John A. Dunn", "Roger A. Wood", "Stephen Rabil"}}),    
+                        Bloc("Other Voters",
+                             1 - 0.26221453287197233,
+                             ["John A. Dunn", "Roger A. Wood", "Stephen Rabil"],
+                             {"Black Voters": .248, "Other Voters": .752},
+                             {}
+                             ))
 
 ## White voters vs non-white voters
 Mayor_White = SimParams()
